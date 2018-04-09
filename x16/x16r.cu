5656ea7626b7bf55e6c76368920c86b4dc8fc904
Brian Lee <brian112358@gmail.com>
2018-04-09 11:11:21 -0700

    Added debug code (disabled by default) for measuring shares with each 80/64-byte algo.

--- x16/x16r.cu
+++ x16/x16r.cu
@@ -223,6 +223,12 @@ static bool init[MAX_GPUS] = { 0 };
 #define _DEBUG_PREFIX "x16r-"
 #include "cuda_debug.cuh"
 
+// #define GPU_HASH_CHECK_LOG
+
+#ifdef GPU_HASH_CHECK_LOG
+	static int algo80_tests[HASH_FUNC_COUNT] = { 0 };
+	static int algo64_tests[HASH_FUNC_COUNT] = { 0 };
+#endif
 static int algo80_fails[HASH_FUNC_COUNT] = { 0 };
 
 extern "C" int scanhash_x16r(int thr_id, struct work* work, uint32_t max_nonce, unsigned long *hashes_done)
@@ -529,6 +535,25 @@ extern "C" int scanhash_x16r(int thr_id, struct work* work, uint32_t max_nonce, 
 				} else {
 					pdata[19] = work->nonces[0] + 1; // cursor
 				}
+				#ifdef GPU_HASH_CHECK_LOG
+				gpulog(LOG_INFO, thr_id, "hash found with %s 80!", algo_strings[algo80]);
+
+				algo80_tests[algo80] += work->valid_nonces;
+				char oks64[128] = { 0 };
+				char oks80[128] = { 0 };
+				char fails[128] = { 0 };
+				for (int a = 0; a < HASH_FUNC_COUNT; a++) {
+						const char elem = hashOrder[a];
+						const uint8_t algo64 = elem >= 'A' ? elem - 'A' + 10 : elem - '0';
+						if (a > 0) algo64_tests[algo64] += work->valid_nonces;
+						sprintf(&oks64[strlen(oks64)], "|%X:%2d", a, algo64_tests[a] < 100 ? algo64_tests[a] : 99);
+						sprintf(&oks80[strlen(oks80)], "|%X:%2d", a, algo80_tests[a] < 100 ? algo80_tests[a] : 99);
+						sprintf(&fails[strlen(fails)], "|%X:%2d", a, algo80_fails[a] < 100 ? algo80_fails[a] : 99);
+				}
+				applog(LOG_INFO, "K64: %s", oks64);
+				applog(LOG_INFO, "K80: %s", oks80);
+				applog(LOG_ERR,  "F80: %s", fails);
+				#endif
 				return work->valid_nonces;
 			}
 			else if (vhash[7] > Htarg) {
